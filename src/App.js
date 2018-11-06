f44e0636b8fb3d687009f89e6207101bbcaf1a83
tylenick <tylerlenick@gmail.com>
2018-11-06 10:00:10 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -3,29 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
+import Main from "./pages/Main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
-      </div>
+     <container>
+       <Main />
+      </container>
     );
   }
 }
---
+++ src/pages/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Main;
